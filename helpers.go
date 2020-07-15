5c4042d40b6e02dd6af213c05d581aba46645b32
jxsl13 <jxsl13@gmail.com>
2020-07-15 16:15:52 +0200

    add generation helpers for username and group

--- helpers.go
+++ helpers.go
@@ -10,9 +10,18 @@ import (
 	"github.com/google/uuid"
 )
 
-func generateString() string {
+func generateString(prefix string) string {
 	uuid := uuid.New().String()
-	return strings.ToLower(strings.TrimSpace(strings.Replace(uuid, "-", "", -1)[:30]))
+
+	return prefix + strings.Replace(uuid, "-", "", -1)[:32-len(prefix)]
+}
+
+func generateRandomUser() string {
+	generateString("u")
+}
+
+func generateRandomGroup() string {
+	return generateString("g")
 }
 
 func fileInfo(filePath string) (string, error) {
